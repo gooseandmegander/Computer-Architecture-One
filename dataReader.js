975d860f456b9f02b2a133e5b1f8aa3becaa3315
gooseandmegander <meganawilliamson@gmail.com>
2018-05-14 19:39:01 -0700

    step 7 complete

---
+++ dataReader.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const argv = process.argv.slice(2);
+
+if (argv.length !== 1) {
+    console.log('usage: node [file location] [filename]');
+    process.exit(1);
+}
+
+const filename = argv[0];
+
+const fileData = fs.readFileSync(filename, 'utf8');
+const programLines = fileData.trim().split(/[\r\n]+/g);
+
+const programLinesFormatted = [];
+for (let line of programLines) {
+    if (!isNaN(line.substr(0, 8))) {
+        programLinesFormatted.push(line.substr(0, 8));
+    
+    }
+}
+
+module.exports = { programLinesFormatted }
--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,5 +1,6 @@
 const RAM = require('./ram');
 const CPU = require('./cpu');
+const { programLinesFormatted } = require('../../dataReader');
 
 /**
  * Load an LS8 program into memory
@@ -19,24 +20,9 @@ function loadMemory() {
     //     "00000001"  // HLT       Halt and quit
     // ];
 
-    const program = [ // mult.ls8
-        '10011001', // LDI R0,8  Load R0 with value 8
-        '00000000',
-        '00001000',
-        '10011001', // LDI R1,9  Load R1 with value 9
-        '00000001',
-        '00001001',
-        '10101010', // MUL R0,R1 Multiply R0*R1, storing result in R0
-        '00000000',
-        '00000001',
-        '01000011', // PRN R0    Print value in R0
-        '00000000',
-        '00000001', // HLT       Halt
-    ];
-
     // Load the program into the CPU's memory a byte at a time
-    for (let i = 0; i < program.length; i++) {
-        cpu.poke(i, parseInt(program[i], 2));
+    for (let i = 0; i < programLinesFormatted.length; i++) {
+        cpu.poke(i, parseInt(programLinesFormatted[i], 2));
         // console.log('program:', i, program[i]);
     }
 }
